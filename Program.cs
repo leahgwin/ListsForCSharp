bbe0b98ac0024bdc7cf94af0d031d5150c4922e5
leahgwin <leahgwin@gmail.com>
2018-08-22 13:59:09 -0500

    finished first part of lists exercise

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>() { "Mercury", "Mars" };
+
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+            List<string> lastPlanetsList = new List<string>() { "Uranus", "Neptune" };
+
+            planetList.AddRange(lastPlanetsList);
+            planetList.Insert(1, "Venus");
+            planetList.Insert(2, "Earth");
+            planetList.Add("Pluto");
+
+            // foreach (string planet in planetList)
+            // {
+            //     Console.WriteLine(planet);
+            // }
+
+            List<string> rockyPlanets = planetList.GetRange(0, 4);
+
+            // foreach (string rockyPlanet in rockyPlanets)
+            // {
+            //     Console.WriteLine(rockyPlanet);
+            // }
+
+            planetList.Remove("Pluto");
+
+            foreach (string planet in planetList)
+            {
+                Console.WriteLine(planet);
+            }
+        }
+    }
+}
+
+
